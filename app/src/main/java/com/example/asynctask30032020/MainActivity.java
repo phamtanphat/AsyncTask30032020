10be3e489d26d275fd05f56d6dc1468a52f5a77c
phatdroid94 <phatdroid94@gmail.com>
2020-06-24 20:28:34 +0700

    finish

--- app/src/main/java/com/example/asynctask30032020/MainActivity.java
+++ app/src/main/java/com/example/asynctask30032020/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.asynctask30032020;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -43,13 +46,21 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(Void... voids) {
-            return docNoiDung_Tu_URL("https://khoapham.vn/KhoaPhamTraining/json/tien/demo1.json");
+            return docNoiDung_Tu_URL("https://khoapham.vn/KhoaPhamTraining/json/tien/demo3.json");
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            Log.d("BBB",s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                JSONObject jsonObjectLanguage = jsonObject.getJSONObject("language");
+                JSONObject jsonObjectEn = jsonObjectLanguage.getJSONObject("en");
+                String name = jsonObjectEn.getString("name");
+                Log.d("BBB",name);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 
